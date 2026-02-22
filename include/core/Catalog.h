9eba14bd7c172ef68db1c6409f8ada0f3942497e
Miguel Espinoza <hector.espinoza@utec.edu.pe>
2026-02-21 23:25:59 -0500

    Implementando la clase 'Catalog'

--- include/core/Catalog.h
+++ include/core/Catalog.h
@@ -1,13 +1,22 @@
-//
-// Created by MIGUEL on 21/02/2026.
-//
-
 #ifndef INC_1_CATALOG_H
 #define INC_1_CATALOG_H
 
+#include "../../include/core/Movie.h"
+#include "../../include/text/TextUtils.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
 
 class Catalog {
+private:
+    static bool parse_csv_row(std::istream& in, std::vector<std::string>& row);
+
+public:
+    std::vector<Movie> base_datos;
+    bool load(const std::string& file);
+    void printValidation();
 };
 
 
-#endif //INC_1_CATALOG_H
+#endif
